8e6ce59eba87e14e8d22077b66b99d7dec3e47fe
UEBUNGIOTUNI <Robert.Stuerz@studmail.w-hs.de>
2025-10-23 10:37:46 +0200

    Add files via upload

---
+++ Praktikum 1 Schall, Motor, Ampel/test/Aufgabe4SeperatNeueVersion.ino
@@ -0,0 +1,69 @@
+#include <Servo.h>
+const int SENDEN = 12; 
+const int ECHO = 13; 
+int Entfernung = 0; 
+Servo servoMotor;
+int pos = 0; 
+int servoPin = 16; 
+const int AmpelRot = 5; 
+const int AmpelGelb = 4; 
+const int AmpelGruen = 0; 
+
+void setup()
+ { 
+ Serial.begin(9600);
+ pinMode(SENDEN, OUTPUT);
+ pinMode(ECHO, INPUT);
+ servoMotor.attach(2);
+ servoMotor.write(0);
+ pinMode(AmpelRot, OUTPUT);
+ pinMode(AmpelGelb, OUTPUT);
+ pinMode(AmpelGruen, OUTPUT);
+
+} 
+void loop()
+{ 
+ // Sender kurz ausschalten, um Störungen des Signals zu vermeiden
+ digitalWrite(SENDEN, LOW);
+ delay(5);
+ // Signal für 10 Microsekunden senden, danach wieder ausschalten
+ digitalWrite(SENDEN, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(SENDEN, LOW);
+ // pulseIn -> Zeit messen, bis das Signal zurückkommt
+ long Zeit = pulseIn(ECHO, HIGH);
+ // Entfernung in cm berechnen
+ // Zeit/2 -> nur eine Strecke
+ Entfernung = (Zeit / 2) * 0.03432; 
+ delay(500);
+ 
+ // Messdaten anzeigen
+ Serial.print("Entfernung in cm: ");
+ Serial.println(Entfernung);
+
+
+
+digitalWrite(AmpelRot, HIGH); //Dauerhaft Rot
+servoMotor.write(5); //Schranke zu, kann nicht 95 sein sonst wechselt er von 95 zu 95
+ if (Entfernung < 80) {
+   trafficLightControl(0, 1, 0); //Wenn weniger als 80cm Abstand Gelb 1 Sekunde
+   digitalWrite(AmpelGruen, HIGH); // Dann wird grün geschalten dauerhaft bis delay danach
+   servoMotor.write(95); //Schranke öffnet
+   delay(5000); /delay bevor rot geschalten wird
+   digitalWrite(AmpelRot, HIGH); //wieder Dauerhaft Rot
+  } 
+} 
+
+
+void trafficLightControl(int redDuration, int yellowDuration,
+int greenDuration) {
+ digitalWrite(AmpelRot, HIGH);
+ delay(redDuration * 1000);
+ digitalWrite(AmpelRot, LOW);
+ digitalWrite(AmpelGelb, HIGH);
+ delay(yellowDuration * 1000);
+ digitalWrite(AmpelGelb, LOW);
+ digitalWrite(AmpelGruen, HIGH);
+ delay(greenDuration * 1000);
+ digitalWrite(AmpelGruen, LOW);
+}
